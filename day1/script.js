b6456e8d8c3e688f6678afb44f43c3003114afa9
taccianka <tacciankastudy@gmail.com>
2021-12-04 15:08:00 +0300

    feat: init dom-elements constants

---
+++ day1/script.js
@@ -0,0 +1,8 @@
+const startStopButton = document.querySelector('.startStop');
+const timer = document.querySelector('.timer');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const settings = document.querySelector('.settings');
+const timeValues = document.querySelectorAll('input[type="number"]');
+
+
